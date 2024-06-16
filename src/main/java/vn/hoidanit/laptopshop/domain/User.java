7687e474dc8d37ebc23349782751037669a50294
thanhdeptrai <taothichmi2009@gmail.com>
2024-06-16 09:09:25 +0900

    video 80 One-to-Many

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -1,31 +1,44 @@
 package vn.hoidanit.laptopshop.domain;
 
-import jakarta.persistence.Column;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
-  @Column(name = "email")
   private String email;
 
-  @Column(name = "password")
   private String password;
 
-  @Column(name = "fullName")
   private String fullName;
 
-  @Column(name = "address")
   private String address;
 
-  @Column(name = "phone")
   private String phone;
+
+  private String avatar;
+
+  // roleId
+
+  // User many -> to one -> role
+  @ManyToOne
+  @JoinColumn(name = "role_id")
+  private Role role;
+
+  @OneToMany(mappedBy = "user")
+  List<Order> orders;
 
   public long getId() {
     return id;
@@ -75,10 +88,18 @@ public class User {
     this.phone = phone;
   }
 
+  public String getAvatar() {
+    return avatar;
+  }
+
+  public void setAvatar(String avatar) {
+    this.avatar = avatar;
+  }
+
   @Override
   public String toString() {
     return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName + ", address="
-        + address + ", phone=" + phone + "]";
+        + address + ", phone=" + phone + ", avatar=" + avatar + "]";
   }
 
 }
