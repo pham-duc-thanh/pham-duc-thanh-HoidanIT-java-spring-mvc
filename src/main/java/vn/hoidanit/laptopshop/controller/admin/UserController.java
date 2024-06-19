a774078900947aea766d245f1238c8ef55076695
thanhdeptrai <taothichmi2009@gmail.com>
2024-06-19 19:27:17 +0900

    Video 92 hoàn thiện tính năng upload File phần 2

--- src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
@@ -3,6 +3,7 @@ package vn.hoidanit.laptopshop.controller.admin;
 import org.springframework.web.multipart.MultipartFile;
 
 import jakarta.servlet.ServletContext;
+import vn.hoidanit.laptopshop.service.UploadService;
 import vn.hoidanit.laptopshop.service.UserService;
 import vn.hoidanit.laptopshop.domain.User;
 
@@ -29,13 +30,11 @@ public class UserController {
 
   // Không nên tiêm phụ thuộc @Autwired
   private final UserService userService;
+  private final UploadService uploadService;
 
-  private final ServletContext servletContext;
-
-  public UserController(UserService userService, ServletContext servletContext) {
+  public UserController(UserService userService, UploadService uploadService) {
     this.userService = userService;
-    this.servletContext = servletContext;
-
+    this.uploadService = uploadService;
   }
 
   @RequestMapping("/")
@@ -83,24 +82,8 @@ public class UserController {
       @ModelAttribute("newUser") User hoidanit,
       @RequestParam("hoidanitFile") MultipartFile file) {
 
-    try {
-      byte[] bytes = file.getBytes();
+    String avatar = this.uploadService.handleSaveUploadFile(file, "avatar");
 
-      String rootPath = this.servletContext.getRealPath("/resources/images");
-      File dir = new File(rootPath + File.separator + "avatar");
-      if (!dir.exists())
-        dir.mkdirs();
-      // Create the file on server
-      File serverFile = new File(dir.getAbsolutePath() + File.separator +
-          +System.currentTimeMillis() + "-" + file.getOriginalFilename());
-      BufferedOutputStream stream = new BufferedOutputStream(
-          new FileOutputStream(serverFile));
-      stream.write(bytes);
-      stream.close();
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
     System.out.println("run here " + hoidanit);
     // this.userService.handSaveUser(hoidanit);
     return "redirect:/admin/user";
