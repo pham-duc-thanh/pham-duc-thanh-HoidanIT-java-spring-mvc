4f58fa86f5ddb700ac3c7b920d034a651fe1dc9c
thanhdeptrai <taothichmi2009@gmail.com>
2024-06-29 16:23:45 +0900

    #116

--- src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package vn.hoidanit.laptopshop.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -27,16 +28,27 @@ public class SecurityConfiguration {
     return new CustomUserDetailsService(userService);
   }
 
+  // @Bean
+  // public AuthenticationManager authenticationManager(HttpSecurity http,
+  // PasswordEncoder passwordEncoder,
+  // UserDetailsService userDetailsService) throws Exception {
+  // AuthenticationManagerBuilder authenticationManagerBuilder = http
+  // .getSharedObject(AuthenticationManagerBuilder.class);
+  // authenticationManagerBuilder
+  // .userDetailsService(userDetailsService)
+  // .passwordEncoder(passwordEncoder);
+  // return authenticationManagerBuilder.build();
+  // }
+
   @Bean
-  public AuthenticationManager authenticationManager(HttpSecurity http,
+  public DaoAuthenticationProvider authProvider(
       PasswordEncoder passwordEncoder,
-      UserDetailsService userDetailsService) throws Exception {
-    AuthenticationManagerBuilder authenticationManagerBuilder = http
-        .getSharedObject(AuthenticationManagerBuilder.class);
-    authenticationManagerBuilder
-        .userDetailsService(userDetailsService)
-        .passwordEncoder(passwordEncoder);
-    return authenticationManagerBuilder.build();
+      UserDetailsService userDetailsService) {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService);
+    authProvider.setPasswordEncoder(passwordEncoder);
+    // authProvider.setHideUserNotFoundExceptions(false);
+    return authProvider;
   }
 
 }
